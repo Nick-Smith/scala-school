44f192222b113500f318b78ce39d42f92b315e46
Nick-Smith <nicholas.smith@guardian.co.uk>
2014-11-17 13:31:49 +0000

    Updated the Types tutorial..

--- src/main/scala/school/wk3/Types.scala
+++ src/main/scala/school/wk3/Types.scala
@@ -4,53 +4,71 @@ object Animals {
 
   // Let's create some Animals!
 
-  object Kitty { // the cat
-    def message = "I go Meow"
+  object Kitty {
+    // the cat
+    def behaviour(isPredator: Boolean): String = if (isPredator) "Get lost!" else "Dinner Time!"
   }
 
-  object Fido { // the dog
-    def message = "I go Woof"
+  object Tweety {
+    // the canary
+    def behaviour(isPredator: Boolean): String = if (isPredator) "Run Away!" else "Hey There!"
   }
 
-  object Tweety { // the canary
-    def message = "I go tweet tweet"
-  }
+  // objects are singletons, there is only ever one instance of each object.
 
   // We can generalize this behaviour into a class Animal:
-  class Animal(call: String){ // Note Animal has a constructor which takes the String parameter 'call'.
-    def message = "I go " + call
+  class Animal(isPredator: Boolean) {
+    // Note Animal has a constructor which takes the Boolean parameter 'isPredator'.
+    def behaviour(animal: Animal): String = {
+      if (isPredator)
+        if (animal.isPredator) "Get lost!" else "Dinner Time!"
+      else
+      if (animal.isPredator) "Run Away!" else "Hey There!"
+    }
   }
 
   // Now we can create animal instances using the 'new' keyword to invoke Animal's constructor:
-  val kitty = new Animal("Meow")
-  println(kitty.message)
+  val kittyTheCat = new Animal(true)
 
   // Or we can use inheritance to create objects or classes that extend Animal:
   // A type inherits all of the behaviour of it's parents.
   // A type can only have one parent class (but it may inherit other types as we'll see later).
   // You cannot inherit from an object.
   // In this case Dog is the child and Animal is it's parent.
-  // Dog has inherited the method 'message' from Animal.
-  // It must also call the constructor of Animal.
-  class Dog extends Animal("Woof")
+  // Dog has inherited the method 'behaviour' from Animal.
+  // It must also call the constructor of Animal with the parameter 'isPredator'.
+  class Dog extends Animal(true)
+
   val fido = new Dog()
-  println(fido.message)
 
-  // Similarly Porky the pig inherits the method 'message' from Animal and must call Animal's constructor.
-  object Porky extends Animal("Oink!")
-  println(Porky.message)
+  // We pass the isPredator constructor param from the Fish constructor to the constructor of Animal.
+  class Fish(isPredator: Boolean) extends Animal(isPredator){
 
-  // Companion Objects
-  // You can create an object with the same name as a class. When you do so, this object is called the companion of the class.
-  // Such objects are commonly used to provide alternative constructors for their companion class.
-  object Animal{
+    // We can define alternative constructors using the 'this' keyword
+    def this() = {
+      this(false) // These alternate constructors must ultimately call the primary constructor by invoking 'this'.
+    }
 
-    def apply(call: String): Animal = new Animal(call)
-
-    // Companion objects often have useful general-purpose instances of their companion class
-    val florence = Animal("Moo") // Note we no longer need the 'new' keyword
   }
 
+  val goldy = new Fish()
+  val sharky = new Fish(true)
+
+  // Similarly objects can also inherit behaviour from classes
+  object PorkyThePig extends Animal(false)
+
+  // Companion Objects
+  // You can create an object with the same name as a class, defined in the same file§. When you do so, this object is called the companion of the class.
+  // Such objects are commonly used to provide alternative constructors for their companion class.
+  object Animal {
+
+    def apply(isPredator: Boolean): Animal = new Animal(isPredator)
+
+    // Companion objects often have useful general-purpose instances of their companion class
+    val florenceTheCow = Animal(false) // Note we no longer need the 'new' keyword
+  }
+
+  Animal.florenceTheCow.behaviour(fido)
 
   // Abstract types
   //
@@ -63,25 +81,32 @@ object Animals {
   // A type may only extend one abstract class.
   // Let's create the abstract class Bird. There is no such thing as just a Bird, rather there are specific varieties of birds.
   // But all birds share common behaviour that we want to express in this class
-  abstract class Bird(call: String) extends Animal(call){
-    val flightSpeed: Int // Unimplemented (abstract) val
-    def plumageColour: String // Unimplemented (abstract) method
+  abstract class Bird(isPredator: Boolean) extends Animal(isPredator) {
 
-    def flyMessage = s"I fly at $flightSpeed mpg"
+    // Unimplemented (abstract) val
+    val flightSpeed: Int
 
-    def feathers = s"My feathers are " + plumageColour
+    // Unimplemented (abstract) method
+    def plumageColour: String
+
+    def isFasterThan(speed: Int): Boolean = flightSpeed > speed
+
+    def isHidden(backgroundColour: String): Boolean = backgroundColour == plumageColour
   }
 
   // Now we can create a class that extends Bird.
   // We need to fill in the missing gaps (flightSpeed and plumageColour) but the rest of the behaviour is inherited
-  class Canary extends Bird("tweet tweet"){
+  class Canary extends Bird(false) {
     val flightSpeed = 30
     val plumageColour = "yellow" // Note we can turn a def into a val here!
   }
+
   val tweety = new Canary()
-  println(tweety.flyMessage)
-  println(tweety.feathers)
-  println(tweety.message) // Note tweety still inherits the 'message' method from Animal since Canary < Bird < Animal.
+  tweety.isFasterThan(10)
+  tweety.isHidden("Green")
+  tweety.behaviour(kittyTheCat)
+
+  // Note tweety still inherits the 'behaviour' method from Animal since Canary < Bird < Animal.
 
   // Traits are another abstract type.
   // Traits do not have constructors.
@@ -89,47 +114,51 @@ object Animals {
   // Traits can only extend other traits.
   trait Swimmer {
     val swimSpeed: Int
-    def swimMessage = s"I can swim $swimSpeed mph"
+
+    def swimsFasterThan(speed: Int): Boolean = swimSpeed > speed
   }
 
   trait Jumper {
     def jumpDistance: Int
-    def jumpMessage = s"I can jump $jumpDistance feet"
+
+    def jumpsFartherThan(distance: Int): Boolean = jumpDistance > distance
   }
 
-  object Frog extends Swimmer with Jumper { //Frogs can jump and Swim!
+  object Frog extends Swimmer with Jumper {
+    //Frogs can jump and Swim!
     val jumpDistance = 5
     val swimSpeed = 10
   }
-  println(Frog.jumpMessage)
-  println(Frog.swimMessage)
+
+  Frog.jumpsFartherThan()
+  Frog.swimsFasterThan(10)
 
   // But wait! WHAT ABOUT THE PENGUIN????!
   // Penguins are birds that can swim but can't fly!
-  // Sure we could set the flySpeed to 0 but that still gives an unhelpful message.
+  // Sure we could set the flySpeed to 0 but that still gives an unhelpful result for isFasterThan.
 
-  // Solution - override the flyMessage method!
-  class Penguin extends Bird("...") with Swimmer{
+  // Solution: override the flyMessage method!
+  class Penguin extends Bird(true) with Swimmer {
     val swimSpeed = 20
     val flightSpeed = 0
     val plumageColour = "black and white"
-    override def flyMessage = "I can't fly!"
+
+    override def isFasterThan(speed: Int) = swimsFasterThan(speed)
   }
 
   // Anonymous classes
   // Sometimes you just want an instance of an abstract class or trait without going to the effort of explicitly making one.
   // Scala lets you do this by creating an anonymous class with the 'new' keyword.
-  val mrFish = new Swimmer{
+  val mrFish = new Swimmer {
     val swimSpeed = 30
   }
 
-  val duck = new Bird("quack") with Swimmer{
+  val duck = new Bird(false) with Swimmer {
     val flightSpeed = 50
     val swimSpeed = 10
     val plumageColour = "brown and black"
   }
 }
-
 
 // Properties and Case Classes
 object RationalNumbers {
@@ -137,26 +166,40 @@ object RationalNumbers {
   // Sometimes you want a class that holds properties you can read and possibly change.
   // Ass an example, let's consider rational numbers
   // You could represent a rational number using the class below:
-  class RationalNum(n: Int, d: Int){ // n and d are only accessible within the class
-    val numerator = n // Create an accessor for numerator
+  class RationalNum(n: Int, d: Int) {
+    // n and d are only accessible within the class
+    val numerator = n
+
+    // Create an accessor for denominator
     val denominator = d // Create an accessor for denominator
   }
+
   val half = new RationalNum(1, 2)
   val halfFloat = half.numerator.toFloat / half.denominator.toFloat
 
   // Scala offers a simpler way to achieve this with Properties.
-  class RationalVals(val numerator: Int, val denominator: Int) // the val keyword here makes the constructor arg publicly visible
+  class RationalVals(val numerator: Int, val denominator: Int)
+
+  // the val keyword here makes the constructor arg publicly visible
   val third = new RationalVals(1, 3)
   val thirdFloat = third.numerator.toFloat / third.denominator.toFloat
+
+  // Note you can also use the var keyword in a constructor to make a property both readable and writeable (but don't!).
 
   // Case classes are a special type of class which give you a few things for free:
   // - all constructor params are automatically made into val properties
   // - A hashcode and equals method are automatically generated which compare all properties.
   // - A toString() is generated which includes all properties
   // - A special constructor method is created which doesn't require the 'new' keyword
-  case class Rational(numerator: Int, denominator: Int)
+  // - A copy method which allows you to make a copy of the case class with specific modifications.
+  case class Rational(numerator: Int, denominator: Int) {
 
-  val twoThirds = Rational(2, 3)//Special constructor
+    // We can use the copy method to implement a '*' method which returns a new Rational.
+    def *(i: Int): Rational = copy(numerator * i)
+  }
+
+  val twoThirds = Rational(2, 3)
+  //Special constructor
   val twoThirdsFloat = twoThirds.numerator.toFloat / twoThirds.denominator.toFloat
 
 }
@@ -173,7 +216,6 @@ object RationalNumbers {
 // 1) Write a new function in Animal that prints out the animal's message.
 //    Will this function work for all of the objects and vals specified in Animals?
 //
-// 2) How would you add the methods + - * and / to all of the Rational classes and objects above?
-//    Your solution should only require one definition of each method, not one definition per class.
+// 2) How would you add a 'toFloat' method to all of the Rational classes above?
+// Your solution should only require one definition of the toFloat method, not one per class.
 //
-// 2)
